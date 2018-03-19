242779966ad0649dada9da99aae609b15fe4a675
Derrick Cross <decross1@me.com>
2018-03-18 21:22:26 -0700

    solution for nato alpha

---
+++ nato-alpha.py
@@ -0,0 +1,19 @@
+letters =  {
+    "A": "Alpha",  "B": "Bravo",   "C": "Charlie",
+    "D": "Delta",  "E": "Echo",    "F": "Foxtrot",
+    "G": "Golf",   "H": "Hotel",   "I": "India",
+    "J": "Juliett","K": "Kilo",    "L": "Lima",
+    "M": "Mike",   "N": "November","O": "Oscar",
+    "P": "Papa",   "Q": "Quebec",  "R": "Romeo",
+    "S": "Sierra", "T": "Tango",   "U": "Uniform",
+    "V": "Victor", "W": "Whiskey", "X": "X-ray",
+    "Y": "Yankee", "Z": "Zulu"
+  }
+  
+def nato(word):
+    word = word.upper()
+    string = []
+    for letter in word:
+        if letter in letters:
+            string.append(letters[letter])
+    return str(' '.join(string))
